7f5a5f78cad1dab333fcf0a4c987de82c216fda8
hurbes <banerjeearnab38@gmail.com>
2024-10-26 00:54:25 +0530

    add startup animation

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -4,7 +4,7 @@ import 'package:eventy/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
-  final _navigationService = locator<NavigationService>();
+  final _routerService = locator<RouterService>();
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
@@ -13,6 +13,6 @@ class StartupViewModel extends BaseViewModel {
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
 
-    _navigationService.replaceWithHomeView();
+    _routerService.replaceWithHomeView();
   }
 }
