08de929d5e4875dc94e3689ae7289556575311de
hurbes <banerjeearnab38@gmail.com>
2024-10-28 07:51:16 +0530

    fixed parseitem override

--- lib/core/repository/event_repository.dart
+++ lib/core/repository/event_repository.dart
@@ -6,7 +6,7 @@ class EventRepository extends Repository<Event> {
   EventRepository() : super();
 
   @override
-  Event parseItem(Map<String, dynamic> json) => Event.fromJson(json['data']);
+  Event parseItem(Map<String, dynamic> json) => Event.fromJson(json);
 
   @override
   List<Event> parseList(List<Map<String, dynamic>> jsonList) {
