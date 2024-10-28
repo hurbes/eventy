9a4aaf8ccdb026e36d6365e6d1cadc7ccdef4b62
hurbes <banerjeearnab38@gmail.com>
2024-10-28 08:55:11 +0530

    add success failour demo bottomsheets

--- lib/core/services/stripe_service.dart
+++ lib/core/services/stripe_service.dart
@@ -1,11 +1,18 @@
+import 'dart:math';
+
+import 'package:eventy/app/app.bottomsheets.dart';
+import 'package:eventy/app/app.locator.dart';
 import 'package:eventy/core/mixins/logger_mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 //  'pi_3QEbYCDGwbbsfVRh1a3wmwTl_secret_kMUHmfqeZPcK7L1CnrFWUNftd',
 
 class StripeService with AppLogger {
+  final _bottomSheetService = locator<BottomSheetService>();
+
   Future<void> createPayment(String clientSecret) async {
     logI('Creating payment with client secret: $clientSecret');
 
@@ -28,7 +35,28 @@ class StripeService with AppLogger {
       ),
     );
 
-    Stripe.instance.presentPaymentSheet();
+    await presentPaymentSheet();
+  }
+
+  Future<void> presentPaymentSheet() async {
+    try {
+      final result = await Stripe.instance.presentPaymentSheet();
+      logI('Payment result: $result');
+      _bottomSheetService.showCustomSheet(
+        variant: BottomSheetType.paymentSuccess,
+      );
+    } catch (e) {
+      logE(e.toString());
+      if (Random().nextBool()) {
+        _bottomSheetService.showCustomSheet(
+          variant: BottomSheetType.paymentFail,
+        );
+      } else {
+        _bottomSheetService.showCustomSheet(
+          variant: BottomSheetType.paymentSuccess,
+        );
+      }
+    }
   }
 
   @override
