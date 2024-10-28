923e55d74417c408a0d6b4a8327464fad3a1c59d
hurbes <banerjeearnab38@gmail.com>
2024-10-28 13:59:33 +0530

    Fixed stript android setup

--- android/app/src/main/kotlin/love/sparktest/eventy/MainActivity.kt
+++ android/app/src/main/kotlin/love/sparktest/eventy/MainActivity.kt
@@ -1,5 +1,7 @@
 package love.sparktest.eventy
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
